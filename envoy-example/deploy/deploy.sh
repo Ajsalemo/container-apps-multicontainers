560f68ea5fbce0eba3945dcdb4c2e9edcf9236df
Anthony Salemo <ajssalemo@gmail.com>
2024-03-06 10:42:26 -0500

    added arm templates for deployment

---
+++ envoy-example/deploy/deploy.sh
@@ -0,0 +1,13 @@
+az deployment group create \
+  --resource-group "$RESOURCE_GROUP" \
+  --template-file ./arm.json \
+  --parameters \
+      environment_name="$CONTAINERAPPS_ENVIRONMENT" \
+      location="$LOCATION" \
+      azureContainerRegistryServerUrl="$AZURE_CONTAINER_REGISTRY_SERVER_URL" \
+      azureContainerRegistryPassword="$AZURE_CONTAINER_REGISTRY_PASSWORD" \
+      azureContainerRegistryUsername="$AZURE_CONTAINER_REGISTRY_USERNAME" \
+      azureContainerRegistryEnvoyImageName="$AZURE_CONTAINER_REGISTRY_ENVOY_IMAGE_NAME" \
+      azureContainerRegistryEnvoyImageTag="$AZURE_CONTAINER_REGISTRY_ENVOY_IMAGE_TAG" \
+      azureContainerRegistryGoImageName="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_NAME" \
+      azureContainerRegistryGoImageTag="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_TAG"
---
+++ nginx-example/deploy/deploy.sh
@@ -0,0 +1,13 @@
+az deployment group create \
+  --resource-group "$RESOURCE_GROUP" \
+  --template-file ./arm.json \
+  --parameters \
+      environment_name="$CONTAINERAPPS_ENVIRONMENT" \
+      location="$LOCATION" \
+      azureContainerRegistryServerUrl="$AZURE_CONTAINER_REGISTRY_SERVER_URL" \
+      azureContainerRegistryPassword="$AZURE_CONTAINER_REGISTRY_PASSWORD" \
+      azureContainerRegistryUsername="$AZURE_CONTAINER_REGISTRY_USERNAME" \
+      azureContainerRegistryNginxImageName="$AZURE_CONTAINER_REGISTRY_NGINX_IMAGE_NAME" \
+      azureContainerRegistryNginxImageTag="$AZURE_CONTAINER_REGISTRY_NGINX_IMAGE_TAG" \
+      azureContainerRegistryGoImageName="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_NAME" \
+      azureContainerRegistryGoImageTag="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_TAG"
