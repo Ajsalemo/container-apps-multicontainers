6997fc66cc03fe3a840bb407c2e30ddd23c1ade2
Anthony Salemo <ajssalemo@gmail.com>
2024-03-06 11:40:50 -0500

    updating init container usage

--- envoy-example/deploy/deploy.sh
+++ envoy-example/deploy/deploy.sh
@@ -10,4 +10,6 @@ az deployment group create \
       azureContainerRegistryEnvoyImageName="$AZURE_CONTAINER_REGISTRY_ENVOY_IMAGE_NAME" \
       azureContainerRegistryEnvoyImageTag="$AZURE_CONTAINER_REGISTRY_ENVOY_IMAGE_TAG" \
       azureContainerRegistryGoImageName="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_NAME" \
-      azureContainerRegistryGoImageTag="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_TAG"
+      azureContainerRegistryGoImageTag="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_TAG" \
+      azureContainerRegistryInitContainerImageTag="$AZURE_CONTAINER_REGISTRY_INIT_CONTAINER_IMAGE_TAG" \
+      azureContainerRegistryInitContainerImageName="$AZURE_CONTAINER_REGISTRY_INIT_CONTAINER_IMAGE_NAME" \
--- nginx-example/deploy/deploy.sh
+++ nginx-example/deploy/deploy.sh
@@ -10,4 +10,6 @@ az deployment group create \
       azureContainerRegistryNginxImageName="$AZURE_CONTAINER_REGISTRY_NGINX_IMAGE_NAME" \
       azureContainerRegistryNginxImageTag="$AZURE_CONTAINER_REGISTRY_NGINX_IMAGE_TAG" \
       azureContainerRegistryGoImageName="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_NAME" \
-      azureContainerRegistryGoImageTag="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_TAG"
+      azureContainerRegistryGoImageTag="$AZURE_CONTAINER_REGISTRY_GO_IMAGE_TAG" \
+      azureContainerRegistryInitContainerImageTag="$AZURE_CONTAINER_REGISTRY_INIT_CONTAINER_IMAGE_TAG" \
+      azureContainerRegistryInitContainerImageName="$AZURE_CONTAINER_REGISTRY_INIT_CONTAINER_IMAGE_NAME"
